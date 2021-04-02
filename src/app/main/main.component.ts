a7b864e9ac18849ee04ef3a7da19776366831319
MugeraH <bwosihughes24@gmail.com>
2021-04-03 02:13:43 +0300

    add load more functionality to display more gifs

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -9,17 +9,52 @@ import { Observable } from 'rxjs';
 })
 export class MainComponent implements OnInit {
   gifs: GifItem[];
-
+  isSearch: boolean = false;
+  showLess: boolean = false;
+  searchTerm: string;
   constructor(private gifService: GifsService) {}
 
-  getData = () => {
+  getGifs() {
     this.gifService.getGifs().subscribe((data) => {
+      this.showLess = false;
       this.gifs = data.data;
-      console.log(this.gifs);
     });
-  };
+  }
 
+  getMoreGifs() {
+    this.gifService.getMoreGifs().subscribe((data) => {
+      this.showLess = true;
+      this.gifs = data.data;
+    });
+  }
+
+  getSearchedGifs() {
+    this.isSearch = true;
+    this.showLess = false;
+    this.gifService.searchGifs(this.searchTerm).subscribe((data) => {
+      this.gifs = data.data;
+    });
+  }
+  getMoreSearchedGifs() {
+    this.isSearch = true;
+    this.showLess = true;
+    this.gifService.searchMoreGifs(this.searchTerm).subscribe((data) => {
+      this.gifs = data.data;
+    });
+  }
+
+  showSearchedGifs() {
+    this.isSearch = true;
+
+    this.gifs = [];
+  }
+  showTrendingGifs() {
+    this.isSearch = false;
+
+    this.getGifs();
+    this.searchTerm = '';
+  }
   ngOnInit(): void {
-    this.getData();
+    this.getGifs();
   }
 }
--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -8,12 +8,38 @@ import { Observable } from 'rxjs';
 })
 export class GifsService {
   apiKey: string = environment.apiKey;
-  gifImages: [];
 
   constructor(private http: HttpClient) {}
 
   getGifs(): Observable<any> {
-    let data = this.http.get<any>(this.trendingGifUrl);
-    return data;
+    return this.http.get(
+      `https://api.giphy.com/v1/gifs/trending?api_key=${environment.apiKey}&limit=8`
+    );
   }
+  getMoreGifs(): Observable<any> {
+    return this.http.get(
+      `https://api.giphy.com/v1/gifs/trending?api_key=${environment.apiKey}&limit=20`
+    );
+  }
+
+  searchGifs(searchTerm: string): Observable<any> {
+    return this.http.get(
+      `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${environment.apiKey}&limit=5`
+    );
+  }
+  searchMoreGifs(searchTerm: string): Observable<any> {
+    return this.http.get(
+      `https://api.giphy.com/v1/gifs/search?q=${searchTerm}&api_key=${environment.apiKey}&limit=15`
+    );
+  }
+
+  // searchGifs(gifName: string) {
+  //   return this.http
+  //     .get(
+  //       `https://api.giphy.com/v1/gifs/search?q=${gifName}&api_key=${environment.giphyApiKey}&limit=10`
+  //     )
+  //     .subscribe((response: any) => {
+  //       this.gifs.next(response.data);
+  //     });
+  // }
 }
