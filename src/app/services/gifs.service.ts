9535b1d208f58b823e37a1c4adb8b67b5ed24b17
MugeraH <bwosihughes24@gmail.com>
2021-04-03 02:58:18 +0300

    removed unnecessary comments

--- src/app/services/gifs.service.ts
+++ src/app/services/gifs.service.ts
@@ -33,13 +33,5 @@ export class GifsService {
     );
   }
 
-  // searchGifs(gifName: string) {
-  //   return this.http
-  //     .get(
-  //       `https://api.giphy.com/v1/gifs/search?q=${gifName}&api_key=${environment.giphyApiKey}&limit=10`
-  //     )
-  //     .subscribe((response: any) => {
-  //       this.gifs.next(response.data);
-  //     });
-  // }
+
 }
